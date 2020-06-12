9309944133112442066267dcb15f3e05d03ff6a3
mena <mena@localhost.localdomain>
2020-06-12 19:00:25 +0200

    fix bug in favorit componant

--- src/components/Favoritecomp.js
+++ src/components/Favoritecomp.js
@@ -4,33 +4,23 @@ import Axios from "axios";
 const Favoritecomp = ({ productid, userid }) => {
   const [loved, setloved] = useState([]);
 
-
-  useEffect(() => {
-    if (productid && userid) {
-      Axios.get(`http://localhost:5000/products/favorite/${userid}/${productid}`)
-        .then((res) => {
-          // console.log(res.data[0].rate);
-          try {
-            if (res.data[0])
-              setloved(true);
-            else
-              setloved(false);
-          } catch (error) {
-            setloved(false);
-          }
-
-        })
-    }
-  }, []);
-
-
   const changerate = (e) => {
-    const { target: { className } } = e
     setloved(!loved);
-    console.log(className);
-    Axios.post('http://localhost:5000/products/favorite', { "user": userid, "product": productid }).then((messages) => { console.log(messages); });
+    Axios.post('http://localhost:5000/products/favorite', { "user": userid, "product": productid });
   }
   if (productid && userid) {
+    Axios.get(`http://localhost:5000/products/favorite/${userid}/${productid}`)
+      .then((res) => {
+        try {
+          if (res.data[0])
+            setloved(true);
+          else
+            setloved(false);
+        } catch (error) {
+          setloved(false);
+        }
+
+      });
     return (
       <div className="container col-12">
         <span id="love" className={loved != true ? "fa fa-heart" : "fa fa-heart favorite"} onClick={changerate}></span>
